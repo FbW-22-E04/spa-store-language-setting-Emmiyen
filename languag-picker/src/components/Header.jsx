8270b663cd9da467d09812965d5aab1e72813d1e
Danica Yener <ny21037@googlemail.com>
2022-10-24 19:17:21 +0200

    Language

---
+++ languag-picker/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LanguageChooser from './LanguageChooser'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <p>Please select your Language</p>
+      <LanguageChooser />
+    </div>
+  )
+}
+
+export default Header
